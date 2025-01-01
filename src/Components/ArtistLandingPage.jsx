5fb60c4fc688297f8d3b630de966b39a6df0eff0
vivek kapal <97444828+vivekkkapal@users.noreply.github.com>
2025-01-01 15:28:11 +0000

    updated

--- src/Components/ArtistLandingPage.jsx
+++ src/Components/ArtistLandingPage.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 
 const ArtistLandingPage = () => {
+
+
+  const applemusic = "https://music.apple.com/us/album/1787549751?i=1787549754&app=music";
+  const spotifymusic = "https://open.spotify.com/track/6iWAgZJlr5k23xbgLLv2l7?referral=labelaffiliate&utm_source=1011lA7pCIDx&utm_medium=Indie_TuneCore&utm_campaign=labelaffiliate";
+  const YouTube = "https://www.youtube.com/watch?v=d9aN6s-LiJE";
+  const iTunes = "https://music.apple.com/us/album/launde-pareshaan/1787549751?i=1787549754";
+  const YouTubeMusic = "https://music.youtube.com/watch?v=5iYJEd3A3Aw&si=kdF9Bw_25LOw9T28";
+  const Amazon = "https://www.amazon.com/dp/B0DRPH7T33";
+  const Deezer = "https://www.deezer.com/soon";
+  const Tidal = "https://listen.tidal.com/track/408217725";
+
+  //   const services = [
+//     { name: "Apple Music", link: "#", type: "Play" },
+//     { name: "Spotify", link: "#", type: "Play" },
+//     { name: "YouTube", link: "#", type: "Play" },
+//     { name: "iTunes Store", link: "#", type: "Download" },
+//     { name: "YouTube Music", link: "#", type: "Play" },
+//     { name: "Amazon Music", link: "#", type: "Play" },
+//     { name: "SoundCloud", link: "#", type: "Play" },
+//     { name: "Deezer", link: "#", type: "Play" },
+//     { name: "Tidal", link: "#", type: "Play" },
+//   ];
+ 
+
+
   const services = [
-    { name: "Apple Music", link: "#", type: "Play" },
-    { name: "Spotify", link: "#", type: "Play" },
-    { name: "YouTube", link: "#", type: "Play" },
-    { name: "iTunes Store", link: "#", type: "Download" },
-    { name: "YouTube Music", link: "#", type: "Play" },
-    { name: "Amazon Music", link: "#", type: "Play" },
-    { name: "SoundCloud", link: "#", type: "Play" },
-    { name: "Deezer", link: "#", type: "Play" },
-    { name: "Tidal", link: "#", type: "Play" },
+    { name: "Apple Music", link: applemusic, type: "Play" },
+    { name: "Spotify", link: spotifymusic, type: "Play" },
+    { name: "YouTube", link: YouTube, type: "Play" },
+    { name: "iTunes Store", link: iTunes, type: "Download" },
+    { name: "YouTube Music", link: YouTubeMusic, type: "Play" },
+    { name: "Amazon Music", link: Amazon, type: "Play" },
+    { name: "Deezer", link: Deezer, type: "Play" },
+    { name: "Tidal", link: Tidal, type: "Play" },
   ];
 
   return (
@@ -42,6 +66,7 @@ const ArtistLandingPage = () => {
             >
                 <span className="text-sm sm:text-lg">{service.name}</span>
                 <a
+                target="_"
                 href={service.link}
                 className="px-3 sm:px-4 py-1 sm:py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-500 text-sm sm:text-base"
                 >
